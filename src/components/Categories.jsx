e402bbaf776d6bd1011289a39defed35100e808d
Farid Amimri <farid.amimri@gmail.com>
2023-03-02 11:59:03 +0100

    Design Categories & CategoryItem

---
+++ src/components/Categories.jsx
@@ -0,0 +1,171 @@
+/** @format */
+
+import styled from 'styled-components'
+import ElementIcon from '../images/element.png'
+import IconButton from '@mui/material/IconButton'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import { categorieItems } from '../data'
+import CategoryItem from './CategoryItem'
+import { useState } from 'react'
+
+function Categories() {
+  const slideListLength = categorieItems.length
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const handleArrow = (direction) => {
+    if (direction === 'left') {
+      setCurrentSlide(
+        currentSlide === 0 ? slideListLength - 1 : currentSlide - 1
+      )
+    }
+    if (direction === 'right') {
+      setCurrentSlide(
+        currentSlide === slideListLength - 1 ? 0 : currentSlide + 1
+      )
+    }
+  }
+  return (
+    <Container>
+      <Top>
+        <Left>
+          <AnimationContainer style={{ width: '90px', height: '90px' }}>
+            <Image src={ElementIcon} alt='icône étoile' />
+          </AnimationContainer>
+          <Carousel currentSlide={currentSlide}>
+            {categorieItems.map((item) => (
+              <CategoryItem key={item.id} item={item} id={item} />
+            ))}
+          </Carousel>
+        </Left>
+
+        <Right>
+          <Wrapper>
+            <Title>Des solutions pour tous</Title>
+            <ArrowContainer>
+              <LeftArrow>
+                <IconButton
+                  direction='left'
+                  onClick={() => handleArrow('left')}
+                  style={{ backgroundColor: '#D2EDA8', opacity: '45%' }}
+                >
+                  <ChevronLeftIcon />
+                </IconButton>
+              </LeftArrow>
+              <RightArrow>
+                <IconButton
+                  direction='right'
+                  onClick={() => handleArrow('right')}
+                  style={{ backgroundColor: '#D2EDA8' }}
+                >
+                  <ChevronRightIcon />
+                </IconButton>
+              </RightArrow>
+            </ArrowContainer>
+          </Wrapper>
+        </Right>
+      </Top>
+
+      <Bottom>
+        <AnimationContainer style={{ width: '79px', height: '79px' }}>
+          <Image src={ElementIcon} alt='icône étoile' />
+        </AnimationContainer>
+      </Bottom>
+    </Container>
+  )
+}
+
+export default Categories
+
+const Container = styled.div`
+  background-color: var(--background-color-primary);
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`
+const Top = styled.div`
+  display: flex;
+`
+
+const Left = styled.div`
+  flex: 3;
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  overflow: hidden;
+`
+
+const AnimationContainer = styled.div``
+
+const Image = styled.img`
+  height: 100%;
+  object-fit: cover;
+`
+
+const Carousel = styled.div`
+  display: flex;
+  margin: 40px 0;
+  align-items: flex-end;
+  overflow: hidden;
+  transform: translateX(${(props) => props.currentSlide * 20}vw);
+  transition: all 1.5s ease-in-out;
+
+  #Cheveux {
+    height: 314px;
+  }
+  #Visage {
+    height: 390px;
+  }
+  #Peau {
+    height: 470px;
+  }
+`
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const Wrapper = styled.div`
+  padding: 40px;
+  position: relative;
+`
+
+const Title = styled.h2`
+  color: var(--text-color-tertiary);
+  font-size: 45px;
+  font-weight: 400;
+  margin-bottom: 60px;
+`
+
+const ArrowContainer = styled.div`
+  width: 107px;
+  height: 42px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const LeftArrow = styled.div`
+  display: flex;
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+`
+
+const RightArrow = styled.div`
+  display: flex;
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+`
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 90%;
+`
---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,59 @@
+/** @format */
+
+import styled from 'styled-components'
+import Button from '@mui/material/Button'
+import { Link } from 'react-router-dom'
+
+function CategoryItem({ item }) {
+  return (
+    <Container id={item.category}>
+      <Link>
+        <Image src={item.image} />
+        <Details>
+          <Title>{item.title}</Title>
+          <ButtonContainer>
+            <Button variant='contained' color='success'>
+              VOIR
+            </Button>
+          </ButtonContainer>
+        </Details>
+      </Link>
+    </Container>
+  )
+}
+
+export default CategoryItem
+
+const Container = styled.div`
+  position: relative;
+  width: 300px;
+  border-radius: 35px;
+  margin-right: 20px;
+  overflow: hidden;
+`
+
+const Image = styled.img`
+  height: 100%;
+  object-fit: cover;
+`
+
+const Details = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  color: white;
+`
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+`
+
+const ButtonContainer = styled.div`
+  margin-bottom: 20px;
+`
