43315e7d449b84a546e0c65690eac77259dae4cb
Farid Amimri <farid.amimri@gmail.com>
2023-03-02 11:59:23 +0100

    Add Categories section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 /** @format */
 
 import Navbar from '../components/Navbar'
+import Intro from '../components/Intro'
+import Popular from '../components/Popular'
+import Categories from '../components/Categories'
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Intro />
+      <Popular />
+      <Categories />
     </div>
   )
 }
