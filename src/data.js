0b7a8cff04185e6d505db57081250637f6d6c0bb
Farid Amimri <farid.amimri@gmail.com>
2023-03-02 12:01:37 +0100

    Create data file

---
+++ src/data.js
@@ -0,0 +1,40 @@
+/** @format */
+
+export const sliderItems = [
+  {
+    image: 'assets/slides/slide1.jpg',
+    title: 'Huile Stimulante',
+    price: 3.19
+  },
+  {
+    image: 'assets/slides/slide2.jpg',
+    title: 'Huile Adoucissante',
+    price: 3.49
+  },
+  {
+    image: 'assets/slides/slide3.jpg',
+    title: 'Huile Hydratante',
+    price: 5.89
+  }
+]
+
+export const categorieItems = [
+  {
+    id: 1,
+    image: 'assets/categories/categorie1.jpg',
+    title: 'HAIR',
+    category: 'Cheveux'
+  },
+  {
+    id: 2,
+    image: 'assets/categories/categorie2.jpg',
+    title: 'FACE',
+    category: 'Visage'
+  },
+  {
+    id: 3,
+    image: 'assets/categories/categorie3.jpg',
+    title: 'BODY',
+    category: 'Peau'
+  }
+]
