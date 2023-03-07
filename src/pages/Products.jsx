4370e7a8bd584870a69ebbe0e4ac4f0a2fadf5f7
Farid Amimri <farid.amimri@gmail.com>
2023-03-07 18:10:32 +0100

    Add Category design

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,11 +4,45 @@ import styled from 'styled-components'
 import Navbar from '../components/Navbar'
 import ProductList from '../components/ProductList'
 import Footer from '../components/Footer'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import { useState } from 'react'
 
 function Products() {
+  const [type, setType] = useState('')
+
+  const handleType = (event) => {
+    setType(event.target.value)
+  }
+
   return (
     <Container>
       <Navbar />
+      <Title>Huiles Cosmétiques</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filtrer les produits :</FilterText>
+          <FormControl sx={{ minWidth: 120 }} size='small'>
+            <InputLabel id='type-sort' color='success'>
+              Type
+            </InputLabel>
+            <Select
+              labelId='type-sort'
+              id='type-sort'
+              value={type}
+              label='Type'
+              color='success'
+              onChange={handleType}
+            >
+              <MenuItem value='Cheveux'>Cheveux</MenuItem>
+              <MenuItem value='Visage'>Visage</MenuItem>
+              <MenuItem value='Corps'>Corps</MenuItem>
+            </Select>
+          </FormControl>
+        </Filter>
+      </FilterContainer>
       <ProductList />
       <Footer />
     </Container>
@@ -18,3 +52,23 @@ function Products() {
 export default Products
 
 const Container = styled.div``
+
+const Title = styled.h1`
+  margin: 20px;
+  text-align: center;
+  color: var(--text-color-secondary);
+`
+
+const FilterContainer = styled.div``
+
+const Filter = styled.div`
+  display: flex;
+  margin: 20px;
+  gap: 10px;
+  align-items: center;
+`
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+`
