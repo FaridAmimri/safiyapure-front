cec4917cccb7a0eae25b95ca8dbebbe97a1940fd
Farid Amimri <farid.amimri@gmail.com>
2023-03-08 18:37:51 +0100

    Add routes to Register & Login pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import Product from './pages/Product'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<Products />} />
         <Route path='/product' element={<Product />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   )
