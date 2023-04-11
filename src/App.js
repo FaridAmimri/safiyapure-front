01dab8ed83d98ff67b3f11ec208e63d9ce6f2765
Farid Amimri <farid.amimri@gmail.com>
2023-04-11 18:29:57 +0200

    Update routes and add user condition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 /** @format */
 
 import './global.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import Product from './pages/Product'
@@ -10,14 +15,19 @@ import Login from './pages/Login'
 import Cart from './pages/Cart'
 
 function App() {
+  const user = true
+
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/product' element={<Product />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/products/:category' element={<Products />} />
+        <Route path='/product/:id' element={<Product />} />
+        <Route
+          path='/register'
+          element={user ? <Navigate to='/' /> : <Register />}
+        />
+        <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
         <Route path='/cart' element={<Cart />} />
       </Routes>
     </Router>
