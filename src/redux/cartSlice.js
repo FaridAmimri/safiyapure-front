e63a4e73599be5919c13b7136e7506c3a89ca208
Farid Amimri <farid.amimri@gmail.com>
2023-04-17 11:40:57 +0200

    Add deleteProduct reducer

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -14,9 +14,16 @@ const cartSlice = createSlice({
       state.quantity += 1
       state.products.push(action.payload)
       state.total += action.payload.price * action.payload.quantity
+    },
+    deleteProduct: (state, action) => {
+      state.quantity -= 1
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload._id
+      )
+      state.total -= action.payload.price * action.payload.quantity
     }
   }
 })
 
-export const { addProduct } = cartSlice.actions
+export const { addProduct, deleteProduct } = cartSlice.actions
 export default cartSlice.reducer
