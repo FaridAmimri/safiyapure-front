91a66022072612b7e5b8689b5a4191a3afa17d84
Farid Amimri <farid.amimri@gmail.com>
2023-04-19 10:30:32 +0200

    create apiCalls file

---
+++ src/redux/apiCalls.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import { loginStart, loginSuccess, loginFailure } from './userSlice'
+import { publicRequest } from '../requests'
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart())
+  try {
+    const res = await publicRequest.post('/auth/login', user)
+    dispatch(loginSuccess(res.data))
+  } catch (error) {
+    dispatch(loginFailure())
+  }
+}
