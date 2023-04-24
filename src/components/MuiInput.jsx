a0bbc0d019750cc0c0129814d97c02a3e06ebaf5
Farid Amimri <farid.amimri@gmail.com>
2023-04-24 13:13:52 +0200

    Update classname

--- src/components/MuiInput.jsx
+++ src/components/MuiInput.jsx
@@ -5,7 +5,7 @@ import { TextField } from '@mui/material'
 function MuiInput({ name, label, value, onChange, error = null, ...other }) {
   return (
     <TextField
-      className='muiTextField'
+      className='input'
       color='success'
       label={label}
       name={name}
