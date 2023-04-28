b3e31bfadd5f7556e560cdb828aa36d7ccde7e08
Farid Amimri <farid.amimri@gmail.com>
2023-04-28 16:14:13 +0200

    handle accessToken for user request

--- src/requests.js
+++ src/requests.js
@@ -3,8 +3,10 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:5000/api/'
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjQ0ZDdiNDA0YTdlYzExMDExY2NlOSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDM1OTcwOCwiZXhwIjoxNjgwNjE4OTA4fQ.AxubHcvJPSCmXm34i0IXdOrH-ODuUy2d-4IH9yxCMpk'
+
+const user = JSON.parse(localStorage.getItem('persist:root'))?.user
+const currentUser = user && JSON.parse(user).currentUser
+const TOKEN = currentUser?.accessToken
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL
