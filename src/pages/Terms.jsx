9752401bdcd582c578a03e55c9c2658f23a2a037
Farid Amimri <farid.amimri@gmail.com>
2023-05-02 20:27:34 +0200

    Add terms & conditions page

---
+++ src/pages/Terms.jsx
@@ -0,0 +1,381 @@
+/** @format */
+
+import styled from 'styled-components'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function Terms() {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Title>Conditions Générales de Ventes</Title>
+        <SubTitle>Article 1 : Préambule</SubTitle>
+        <Article>
+          <Text>
+            Les présentes conditions générales de vente s’appliquent à tout
+            achat effectué par un internaute / personne physique sur le site
+            Internet www.safiyapurecom.fr auprès de la Société SAFIYAPURE,
+            Auto-Entreprise au registre du commerce et des sociétés
+            LONS-LE-SAUNIER sous le numéro 82751853100013, siège social 50
+            chemin de certaud 39300 CHAMPAGNOLE, France. Email :
+            contact@safiyapurecom.fr - Téléphone : 06 40 40 82 05.
+          </Text>
+          <br />
+          <Text>
+            Tout internaute peut prendre connaissance des Conditions générales
+            de vente sur le site www.safiyapurecom.fr. Ces conditions générales
+            de vente pouvant faire l'objet de modifications, les conditions
+            applicables sont celles en vigueur sur le Site à la date de
+            passation de votre commande. Toute commande suppose l'adhésion aux
+            présentes conditions générales de vente.
+          </Text>
+        </Article>
+
+        <SubTitle>Article 2 : Conditions pour effectuer une commande</SubTitle>
+        <Article>
+          <Text>
+            Vous déclarez être âgé d'au moins 18 ans et avoir la capacité
+            juridique ou être titulaire d'une autorisation parentale vous
+            permettant d'effectuer une commande sur le Site. Safiyapurecom.fr
+            retournera par tout moyen utile à l'adresse du client une
+            confirmation de commande. Toute commande passée au nom ou pour le
+            compte du client est réputée effectuée par lui même, un de ses
+            préposés, ou une personne autorisée à cet engagement.
+          </Text>
+          <br />
+          <Text>
+            Après avoir passé votre commande, Vous recevrez un email qui Vous
+            confirmera les éléments de votre commande. Toute commande vaudra
+            acceptation des prix et descriptions des produits disponibles à la
+            vente. Le contrat sera formé lors de l'émission de la confirmation
+            de votre commande. Nous Vous informerons par email de l'envoi de vos
+            articles. Cet e-mail constituera notre acceptation de votre commande
+            et formera ainsi le contrat de vente entre Vous et Safiyapurecom.fr.
+            Ces étapes de conclusion du contrat s'appliqueront quelle que soit
+            la procédure d'achat sur le Site.
+          </Text>
+        </Article>
+
+        <SubTitle>Article 3 : Prix, disponibilité et livraison</SubTitle>
+        <Article>
+          <Text>
+            Les prix affichés sur le Site sont indiqués en euros, hors
+            participation aux frais de port. Les prix des produits et offres de
+            service, les taxes afférentes, frais de port et les frais annexes
+            sont indiqués sur la facture émise par Safiyapurecom.fr.
+          </Text>
+          <br />
+          <Text>
+            Nous livrons depuis Champagnole en France dans tous les pays du
+            monde.
+          </Text>
+          <br />
+          <Text>
+            Nous nous engageons à vous apporter plus de précisions sur la
+            disponibilité (commande au-delà des stocks). Vous bénéficiez d'une
+            garantie constituant un droit d'échange de 10 jours. Toute opération
+            intervenant entre Safiyapurecom.fr et ses clients non contestée dans
+            les 10 jours suivant l'expédition de Votre commande, ne pourra
+            donner lieu à aucune réclamation.
+          </Text>
+          <br />
+          <Text>
+            Les articles et les photos représentant les produits que nous
+            vendons, sont décrits et présentés avec la plus grande exactitude et
+            n'ont qu'une valeur indicative, en raison des altérations pouvant
+            exister du fait de leur transcription sur le site. Si en dépit de
+            notre vigilance, des erreurs se sont produites, liées à la mise à
+            jour du Site internet et que certains articles soient indisponibles
+            ou mal étiquetés, Nous n'en serions, en aucun cas être tenu pour
+            responsable. Vous en seriez informé par e-mail dans les meilleurs
+            délais.
+          </Text>
+          <br />
+          <Text>
+            En cas d'indisponibilité liée à nos fournisseurs dans les délais
+            indiqués, Nous pourrions être amenés à vous livrer un article de
+            remplacement, de composition légèrement différente à un prix
+            équivalent de celui décrit, mais en tout état de cause de qualité
+            égale ou supérieure à l'article commandé. Dans le cas où Nous ne
+            pourrions pas Vous fournir un article de remplacement de qualité à
+            un prix équivalent, Vous pourrez annuler votre commande
+            partiellement ou en totalité. En cas d'annulation total de votre
+            commande, les frais de retours consécutifs à l'exercice éventuel du
+            droit de rétractation resteront à Votre charge.
+          </Text>
+          <br />
+          <Text>
+            <b>La livraison de vos commandes est assurée par Mondial Relay.</b>
+            Les commandes sont généralement traitées dans un délai de 24H, du
+            lundi au vendredi, avant 15H, (délai à titre indicatif, suivant
+            stock). A compter de la prise en charge par Mondial Relay, vous êtes
+            livré en point relais en 72h sous réserve des heures limites de
+            dépôt. Ne sont pris en compte pour la gestion de votre commande que
+            les jours ouvrés. (hors Dimanche et Jours Fériés)
+          </Text>
+        </Article>
+
+        <SubTitle>Article 4 : Paiement</SubTitle>
+        <Article>
+          <Text>
+            Le paiement de la totalité du prix est dû dès la commande. Le client
+            s'engage à régler le prix stipulé pour le ou les produits commandés
+            sur le site www.safiyapurecom.fr (prix des produits et du
+            transport), ou autres taxes dues à l'occasion de l'importation des
+            produits dans le pays du lieu de livraison.
+          </Text>
+          <br />
+          <Text>
+            Nous acceptons les paiements par carte suivants : Visa, MasterCard,
+            Carte Bleu, Electron... Notre site et toutes les transactions par
+            carte Bancaire sont sécurisées par le système SSL, les serveurs de
+            paiements sécurisés SSL de Stripe vous sont proposés. Nous n'avons
+            aucun accès à vos données bancaires.
+          </Text>
+          <br />
+        </Article>
+
+        <SubTitle>Article 5 : Retour & Politique de rétractation</SubTitle>
+        <Article>
+          <Text>
+            <b>Principe de rétractation</b>
+            <br />
+            Conformément à l’article L-221-18 du code de la consommation et
+            suite à la loi Hamon, le CLIENT dispose par principe du droit de
+            renvoyer ou de restituer le PRODUIT à SAFIYAPURE ou à une personne
+            désignée par ce dernier, sans retard excessif, et au plus tard dans
+            les quatorze (14) jours suivant la communication de sa décision de
+            se rétracter.
+          </Text>
+          <br />
+          <Text>
+            <b>Délai de rétractation</b>
+            <br />
+            Le délai de rétractation expire quatorze (14) jours calendaires
+            après le jour où le CLIENT, ou un tiers autre que le transporteur et
+            désigné par le CLIENT, prend physiquement possession du PRODUIT. Si
+            la commande du CLIENT porte sur plusieurs PRODUITS et si ces
+            PRODUITS sont livrés séparément, le délai de rétractation expire
+            quatorze (14) jours après le jour où le CLIENT, ou un tiers autre
+            que le transporteur et désigné par le CLIENT, prend physiquement
+            possession du dernier PRODUIT.
+          </Text>
+          <br />
+          <Text>
+            <b>Notification du droit de rétractation</b>
+            <br />
+            Pour exercer son droit de rétractation, le CLIENT doit notifier sa
+            décision de se rétracter du présent contrat au moyen d’une
+            déclaration dénuée d’ambiguïté par mail : contact@safiyapurecom.fr
+          </Text>
+          <br />
+        </Article>
+
+        <SubTitle>
+          Article 6 : Réserve de propriété - Défaut de paiement
+        </SubTitle>
+        <Article>
+          <Text>
+            Les produits commandés restent la propriété de Safiyapurecom.fr
+            jusqu'au paiement définitif et intégral de la totalité de leur prix.
+            Safiyapurecom.fr se réserve dès lors le droit de revendiquer les
+            produits commandés en cas de défaut de paiement. Dans cette
+            hypothèse et sur première demande de la part de Safiyapurecom.fr, le
+            client s'engage à restituer tout produit non payé, tous frais à sa
+            charge.
+          </Text>
+          <br />
+        </Article>
+
+        <SubTitle>Article 7 : Réserve informatique</SubTitle>
+        <Article>
+          <Text>
+            Safiyapurecom.fr ne pourrait voir sa responsabilité engagée pour les
+            inconvénients et dommages relatifs à l'utilisation du réseau
+            Internet, tels notamment une rupture dans le service, la présence de
+            virus informatiques ou intrusions extérieures ou plus généralement
+            tous cas qualifiés de force majeure par les tribunaux.
+          </Text>
+          <br />
+        </Article>
+
+        <SubTitle>Article 8 : Garantie et responsabilité</SubTitle>
+        <Article>
+          <Text>
+            Vous bénéficiez des dispositions de la garantie légale des vices
+            cachés. Nous déclinons toute responsabilité dans l'hypothèse où
+            l'article livré ne respecterait pas la législation du pays de
+            livraison. Toutefois, Notre engagement est d'apporter tous les soins
+            en usage dans la profession pour la mise en œuvre du service offert
+            au client. Néanmoins, Notre responsabilité ne pourra pas être
+            retenue en cas de manquement à nos obligations contractuelles du
+            fait d'un cas fortuit ou d'un cas de force majeur tel que défini par
+            la jurisprudence.
+          </Text>
+          <br />
+          <Text>
+            Notre responsabilité ne sera pas engagée en cas de retard dû à une
+            rupture de stock chez l'éditeur ou chez le fournisseur ; ainsi qu'en
+            cas de différences non substantielles entre les photos de
+            présentation des articles sur notre Site, les textes et
+            illustrations des articles commandés.
+          </Text>
+          <br />
+          <Text>
+            Nous mettons en œuvre tous les moyens dont Nous disposons pour
+            assurer les prestations, objets des présentes Conditions générales
+            de vente. En aucun cas, nous ne garantissons que les marchandises
+            fournies soient aptes à répondre à un problème particulier propre à
+            l'activité de l'utilisateur. Nous ne sommes tenus à aucune
+            indemnisation envers l'utilisateur ou envers les tiers pour les
+            conséquences de l'usage des marchandises, qu'il s'agisse de dommages
+            directs ou indirects, d'accidents aux personnes, de dommages à des
+            biens distincts de notre matériel, de pertes de bénéfice ou de
+            manque à gagner, de dommages provenant ou à provenir d'une
+            détérioration. Le client ayant pris connaissance des
+            caractéristiques techniques des produits, a sous sa propre
+            responsabilité et en fonction de ses besoins tels qu'il les a
+            déterminés, porté son choix sur les Produits faisant l'objet de sa
+            commande.
+          </Text>
+          <br />
+        </Article>
+
+        <SubTitle>Article 9 : Licence d'accès au Site</SubTitle>
+        <Article>
+          <Text>
+            Nous vous accordons une licence limitée à l'accès et à l'utilisation
+            du Site pour votre utilisation personnelle. En aucun cas Vous n'êtes
+            autorisé à télécharger ou à modifier tout ou partie de ce Site sans
+            Notre autorisation écrite et expresse. Cette licence ne Vous permet
+            en aucun cas d'utiliser, pour la vente ou pour toute autre
+            utilisation commerciale ce Site ou son contenu (produits listés,
+            descriptions, prix, téléchargement ou copie des informations pour le
+            compte d'un autre commerçant, utilisation de données, logiciels,
+            extraits sonores, graphismes, images, textes, photographies,
+            outils).
+          </Text>
+          <br />
+          <Text>
+            Ce Site ou toute partie de ce Site ne doit en aucun cas être
+            reproduit, copié, vendu ou exploité pour des raisons commerciales
+            sans notre autorisation. Vous ne devez pas utiliser des techniques
+            permettant de copier une marque, un logo ou toute autre information
+            (notamment images, texte, maquettes) dont Nous sommes propriétaires
+            sans notre accord express et écrit. Toute utilisation non autorisée
+            mettra fin à la licence que Nous Vous avons accordée et fera l'objet
+            de poursuites. Certaines photos de produits peuvent être non
+            contractuelles.
+          </Text>
+          <br />
+        </Article>
+
+        <SubTitle>Article 10 : Déclaration de confidentialité</SubTitle>
+        <Article>
+          <Text>
+            Safiyapurecom.fr est soucieux de la protection des données
+            personnelles. Il s’engage à assurer le meilleur niveau de protection
+            à vos données personnelles en conformité avec les réglementations
+            européennes et françaises qui lui sont applicables en matière de
+            protection des données personnelles.
+          </Text>
+          <br />
+          <Text>
+            <b>Nous ne recueillons que les données utiles à son utilisation</b>
+            <br />
+            Vos données sont conservées confidentiellement par Safiyapurecom.fr
+            pour les besoins de vos commandes, de son exécution et dans le
+            respect de la loi. Et, si le client a expressément choisi cette
+            option, lui envoyer des newsletters et/ou des offres
+            promotionnelles, sauf si le client ne souhaite plus recevoir de
+            telles communications de notre part. Les clients peuvent à tout
+            moment se désinscrire en accédant à leur compte ou en cliquant sur
+            le lien hypertexte prévu à cet effet en bas ce chaque offre reçue
+            par courrier électronique.
+          </Text>
+          <br />
+          <Text>
+            Nous nous interdisons toute revente de fichiers, qu'il s'agisse
+            d'adresses postales, d'adresses électroniques, de numéros de
+            téléphone, de télécopie ou de tout autre renseignement. Les données
+            peuvent être communiquées, en tout ou partie, aux prestataires de
+            paiement et de livraison intervenant dans le processus de commande.
+          </Text>
+          <br />
+          <Text>
+            Nous utilisons Google Adwords, Analytics et Bing à des fins
+            statistiques, et pour nos campagnes publicitaires. L’objectif est de
+            déterminer combien de clics sur les publicités ont conduit à des
+            ventes. Nous n’utilisons ces données que dans le but d’analyser le
+            comportement de l’internaute et non de le suivre et le surveiller.
+          </Text>
+          <br />
+          <Text>
+            <b>Nous n'enregistrons pas vos données bancaires</b>
+            <br />
+            Les informations de carte bancaire ne sont pas stockées sur notre
+            boutique en ligne, elles ne passent pas par nos serveurs, elles sont
+            directement traitées par notre partenaire de paiement certifié.
+            Aussi, aucun salarié n’a accès à vos données bancaires.
+          </Text>
+          <br />
+          <Text>
+            <b>Droits & règlement général sur la protection des données</b>
+            <br />
+            Conformément à la loi n°78-17 du 6 janvier 1978 relative à
+            l’informatique aux fichiers et aux libertés et au Règlement (UE)
+            2016/679 du Parlement européen et du Conseil du 27 avril 2016
+            relatif à la protection des personnes physiques à l’égard du
+            traitement des données à caractère personnel et à la libre
+            circulation de ces données, et abrogeant la directive 95/46/CE
+            (Règlement général sur la protection des données dit RGPD),
+            Safiyapurecom.fr assure la mise en oeuvre des droits des personnes
+            concernées.
+          </Text>
+          <br />
+        </Article>
+
+        <SubTitle>Article 11 : Compétence et droit applicable</SubTitle>
+        <Article>
+          <Text>
+            Les présentes Conditions Générales ainsi que les relations entre le
+            Client et SAFIYAPURE sont régies par le droit français. En cas de
+            litige, seuls les tribunaux français seront compétents. Toutefois,
+            préalablement à tout recours au juge arbitral ou étatique, sera
+            privilégiée la négociation dans un esprit de loyauté et de bonne foi
+            en vue de parvenir à un accord amiable lors de la survenance de tout
+            conflit relatif au présent contrat, y compris portant sur sa
+            validité.
+          </Text>
+          <br />
+        </Article>
+      </Container>
+      <Footer />
+    </>
+  )
+}
+
+export default Terms
+
+const Container = styled.div`
+  margin: 20px;
+  padding-top: 50px;
+`
+
+const Title = styled.h2`
+  margin: 20px;
+  color: #2e7d32;
+`
+
+const SubTitle = styled.h3`
+  margin: 20px;
+  margin-top: 30px;
+  color: black;
+`
+
+const Article = styled.div`
+  margin: 20px;
+`
+
+const Text = styled.p``
